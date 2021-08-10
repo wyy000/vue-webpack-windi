b9ca6efe2c4802b4e7b7e45cac7a71dd094e4311
yang <952711716@qq.com>
2021-08-10 16:32:27 +0800

    fix: popper应用手动阻止点击关闭事件

--- src/views/test/title-popper.vue
+++ src/views/test/title-popper.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 popper(:target-el="targetEl" v-model:show="show")
-  div(v-if="show")
+  div(v-if="show" @click.stop="")
     span I'm a title popper. And
-    button(@click="$emit('update')") click me
+    button(@click.stop="$emit('update')") click me
     span is close.
 </template>
 
 <script>
-import {defineComponent, onBeforeUnmount, onMounted, ref, watch} from 'vue'
+import {defineComponent, onBeforeUnmount, ref, watch} from 'vue'
 import Popper from '@/components/popper.vue'
 
 export default defineComponent({
