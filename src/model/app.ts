fba01b0f3d70e053e25ad64dd87f181b2cae0543
wyy000 <952711716@qq.com>
2021-09-05 19:57:24 +0800

    feat: 添加接口

--- src/model/app.ts
+++ src/model/app.ts
@@ -12,6 +12,7 @@ export const list = ref<ListItem[]>([])
 interface Response {
   code: number
   data: any
+  message?: string
 }
 
 const SUCCESS_CODE = 0
@@ -25,10 +26,42 @@ export async function fetchList (params: ListParams): Promise<ListItem[]> {
   params = Object.assign({pageNum: 1, pageSize: 10}, params)
 
   const res = await fetch(`/api/shop${serialize(params)}`)
-  const {code, data} = await res.json() as Response
+  const {code, data, message} = await res.json() as Response
   if (code === SUCCESS_CODE) list.value = data
-  else throw Error(`response error: error code is ${code}`)
+  else throw Error(`response error: error code is ${code}, ${message}`)
 
   return list.value
 }
 
+type Shop = {
+  id: string
+  name: string
+}
+export async function updateShop (params: Shop): Promise<Shop> {
+  const res = await fetch(`/api/shop/${params.id}${serialize(params)}`, {
+    method: 'put',
+    body: JSON.stringify(params),
+  })
+  const {code, data, message} = await res.json() as Response
+  if (code === SUCCESS_CODE) {
+    const index = list.value.findIndex(it => it.id === data.id)
+    list.value[index] = data
+  }
+  else throw Error(`response error: error code is ${code}, ${message}`)
+
+  return data
+}
+
+export async function deleteShop (id: string): Promise<boolean> {
+  const res = await fetch(`/api/shop/${id}`, {
+    method: 'delete',
+  })
+  const {code, message} = await res.json() as Response
+  if (code === SUCCESS_CODE) {
+    const index = list.value.findIndex(it => it.id)
+    list.value.splice(index, 1)
+  }
+  else throw Error(`response error: error code is ${code}, ${message}`)
+
+  return true
+}
--- src/views/app/list.vue
+++ src/views/app/list.vue
@@ -1,14 +1,24 @@
 <template lang="pug">
 h1 Reading list
-div(class="flex flex-col")
-  div(v-for="it in refList") {{it}}
+div(v-if="showList" class="flex flex-col")
+  div(v-for="it in refList" :key="it.id")
+    div {{it.name}}
+    div.flex.justify-center
+      input(v-model="it.name" class="border")
+      button(@click="updateShop(it)") 修改
+      button(@click="fetchDeleteShop(it.id)") 删除
 </template>
 
 <script setup>
 import {computed} from 'vue'
-import {list, fetchList} from '../../model/app'
+import {list, fetchList, updateShop as fetchUpdateShop, deleteShop as fetchDeleteShop} from '../../model/app'
 
 const refList = computed(() => list.value)
+const showList = computed(() => !!list.value.length)
+
+async function updateShop (params) {
+  await fetchUpdateShop(params)
+}
 
 async function queryList () {
   await fetchList()
